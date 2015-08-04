0341cb9ffce7a2d5198fb0adba864cc43733469a
Safak Serdar Kapci <sskapci@gmail.com>
2015-08-05 00:13:20 +0300

    İndirme

---
+++ hadoop.sh
@@ -0,0 +1,16 @@
+echo ""
+echo " _______  _____    _____  _    _   _____ "
+echo "|__   __||  __ \  / ____|| |  | | / ____|"
+echo "   | |   | |__) || (___  | |  | || |  __ "
+echo "   | |   |  _  /  \___ \ | |  | || | |_ |"
+echo "   | |   | | \ \  ____) || |__| || |__| |"
+echo "   |_|   |_|  \_\|_____/  \____/  \_____|"
+echo ""
+echo ""
+echo ""
+echo "Hadoop Workshop"
+echo ""
+echo ""
+
+wget http://ftp.itu.edu.tr/Mirror/Apache/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz -O /home/genel/Downloads/hadoop-2.7.1.tar.gz
+xdg-open http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/R/eclipse-java-mars-R-linux-gtk-x86_64.tar.gz
